d957bac66052f38fa7591bedfa1bba9e35ee0259
eunbi <eunbi5918@naver.com>
2020-08-12 21:20:35 +0900

    first commit

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { NavigationEvents } from 'react-navigation'
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+
+const SignupScreen = ({ navigation }) => {
+    const { state, signin, clearErrorMessage } = useContext(AuthContext)
+
+
+
+    return (
+        <View style={styles.container}>
+            <NavigationEvents
+                onWillFocus={clearErrorMessage}
+            />
+            <AuthForm
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signin}
+            />
+            <NavLink
+                routeName="Signin"
+                text="Already have an account? Sign in instead!"
+
+            />
+
+        </View>
+    )
+
+}
+
+SignupScreen.navigationOptions = () => {
+    return {
+        header: () => false,
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+    },
+
+})
+
+export default SignupScreen
